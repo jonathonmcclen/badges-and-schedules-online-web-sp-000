55a54d9b1b16878168873195842dda2b40bfed1f
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-04-27 20:33:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,12 +14,18 @@ def batch_badge_creator(array)
   new_array
 end
 
+def room_asignement_maker(name, num)
+  "Hello, #{name}! You'll be assigned to room #{num}"
+end
+
 def assign_rooms(array)
-   array.each do |i|
-     
-     
-     
-   end
+   new_array = []
+  
+  array.each do |name|
+    new_badge = room_asignement_maker(name, name + 1)
+    new_array << new_badge
+  end
+  new_array
      
 end
 
